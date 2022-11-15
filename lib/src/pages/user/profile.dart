479488feea6f2ed1f70926f7e3af9cb107174a0a
JoanWu5 <394104840@qq.com>
2022-11-14 19:09:36 -0500

    add user profile page

---
+++ lib/src/pages/user/profile.dart
@@ -0,0 +1,316 @@
+import 'package:demo/constants.dart';
+import 'package:demo/models/user.dart';
+import 'package:demo/src/pages/utils/center_background.dart';
+import 'package:demo/src/pages/utils/icon_picker.dart';
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  final User user;
+  const Profile({super.key, required this.user});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final GlobalKey<FormState> _profileformKey = GlobalKey<FormState>();
+
+  User changedUser = User(id: -1);
+
+  @override
+  void initState() {
+    super.initState();
+    changedUser = widget.user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('Profile'),
+        centerTitle: true,
+        elevation: 0,
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back_outlined));
+          },
+        ),
+        toolbarHeight: topMenuBarHeight,
+        actions: [
+          TextButton(
+              onPressed: () {
+                // TODO save the change
+                if (_profileformKey.currentState!.validate()) {
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Save'))
+        ],
+      ),
+      body: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: CenterBackground(
+          child: SingleChildScrollView(
+            child: Form(
+              key: _profileformKey,
+              child: Column(
+                children: [
+                  IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          iconSize: profileImgHeight,
+                          onPressed: () async {
+                            int? result = await showIconPicker(
+                              context: context,
+                              defalutIconId: changedUser.iconId,
+                              isIcon: false,
+                              svgs: allUserIcons,
+                              crossCount: 3,
+                            );
+                            setState(() {
+                              changedUser.iconId = result!;
+                            });
+                          },
+                          icon: ClipOval(
+                            child: Image.asset(
+                              "$assetUserPath${(changedUser.iconId! + 1).toString()}.png",
+                              width: profileImgWidth,
+                              height: profileImgHeight,
+                              fit: BoxFit.scaleDown,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 2 * defaultPadding),
+                    child: Divider(),
+                  ),
+                  IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 0.5 * defaultPadding),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'FirstName',
+                                style: textMiddleSize,
+                                textAlign: TextAlign.left,
+                              ),
+                              const SizedBox(height: 0.5 * defaultPadding),
+                              Expanded(
+                                child: TextFormField(
+                                  initialValue: changedUser.firstName,
+                                  textInputAction: TextInputAction.next,
+                                  decoration: const InputDecoration(
+                                    hintText: "FirstName",
+                                  ),
+                                  validator: (value) {
+                                    return value!.trim().isNotEmpty
+                                        ? null
+                                        : "please enter firstname";
+                                  },
+                                  onChanged: (value) {
+                                    setState(() {
+                                      changedUser.firstName = value;
+                                    });
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          width: defaultPadding,
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'LastName',
+                                style: textMiddleSize,
+                                textAlign: TextAlign.left,
+                              ),
+                              const SizedBox(height: 0.5 * defaultPadding),
+                              Expanded(
+                                child: TextFormField(
+                                  initialValue: changedUser.lastName,
+                                  textInputAction: TextInputAction.next,
+                                  decoration: const InputDecoration(
+                                    hintText: "LastName",
+                                  ),
+                                  validator: (value) {
+                                    return value!.trim().isNotEmpty
+                                        ? null
+                                        : "please enter lastname";
+                                  },
+                                  onChanged: (value) {
+                                    setState(() {
+                                      changedUser.lastName = value;
+                                    });
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 0.5 * defaultPadding),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 0.5 * defaultPadding),
+                    child: Divider(),
+                  ),
+                  IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 0.5 * defaultPadding),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Description',
+                                style: textMiddleSize,
+                                textAlign: TextAlign.left,
+                              ),
+                              const SizedBox(height: 0.5 * defaultPadding),
+                              Expanded(
+                                child: TextFormField(
+                                  initialValue: changedUser.description,
+                                  textInputAction: TextInputAction.next,
+                                  decoration: const InputDecoration(
+                                    hintText: "Describe about yourself",
+                                  ),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      changedUser.description = value;
+                                    });
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 0.5 * defaultPadding),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 0.5 * defaultPadding),
+                    child: Divider(),
+                  ),
+                  IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 0.5 * defaultPadding),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Email',
+                                style: textMiddleSize,
+                                textAlign: TextAlign.left,
+                              ),
+                              const SizedBox(height: 0.5 * defaultPadding),
+                              Expanded(
+                                child: Text(
+                                  changedUser.email!,
+                                  style: textLargeSize,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 0.5 * defaultPadding),
+                    child: Divider(),
+                  ),
+                  IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 0.5 * defaultPadding),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Phone',
+                                style: textMiddleSize,
+                                textAlign: TextAlign.left,
+                              ),
+                              const SizedBox(height: 0.5 * defaultPadding),
+                              Expanded(
+                                child: TextFormField(
+                                  initialValue: changedUser.phone,
+                                  textInputAction: TextInputAction.done,
+                                  decoration: const InputDecoration(
+                                    hintText: "XXX-XXX-XXXX",
+                                  ),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      changedUser.description = value;
+                                    });
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 0.5 * defaultPadding),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
